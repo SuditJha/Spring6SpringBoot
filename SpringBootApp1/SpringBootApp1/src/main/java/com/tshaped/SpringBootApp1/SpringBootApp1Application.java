e29077b10a2477e89f1dac210d8d9bc9e8b3aa0d
SuditJha <jsudit2@gmail.com>
2025-07-13 22:06:27 +0530

    Life Cycle of Bean

--- SpringBootApp1/SpringBootApp1/src/main/java/com/tshaped/SpringBootApp1/SpringBootApp1Application.java
+++ SpringBootApp1/SpringBootApp1/src/main/java/com/tshaped/SpringBootApp1/SpringBootApp1Application.java
@@ -4,15 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.tshaped.SpringBootApp1.service.Greeting;
+
 @SpringBootApplication // => @ComponentScan
 public class SpringBootApp1Application {
 
 	public static void main(String[] args) {
 	   ConfigurableApplicationContext container = SpringApplication.run(SpringBootApp1Application.class, args);
 	   
-	   
+//	   52 + 1 bean you created
 	  int count = container.getBeanDefinitionCount();
 	  System.out.println("Beans Count in IOC " + count);
+	  
+	  Greeting greet = container.getBean(Greeting.class);
+	  System.out.println(greet.generateWish());
 	}
 
 }
--- SpringBootApp1/SpringBootApp1/src/main/java/com/tshaped/SpringBootApp1/service/Greeting.java
+++ SpringBootApp1/SpringBootApp1/src/main/java/com/tshaped/SpringBootApp1/service/Greeting.java
@@ -2,12 +2,37 @@ package com.tshaped.SpringBootApp1.service;
 
 import org.springframework.stereotype.Service;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Service
 public class Greeting {
+	
+	static {
+		System.out.println("Static Block");
+	}
+	
+	{
+		System.out.println("Non static Java initialization block");
+	}
 
 	public Greeting() {
 		// TODO Auto-generated constructor stub
-		System.out.println("Greetings Bean Created");
+		System.out.println("Greetings Bean Created --> Constructor");
+	}
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("Greeting Bean Init Method");
+	}
+	
+	public String generateWish() {
+		return "Good Morning";
+	}
+	
+	@PreDestroy
+	public void destroy() {
+		System.out.println("Greeting Bean Destroyed");
 	}
 
 }
